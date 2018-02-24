7cb7ae1b0685bfa0c6efb0c31570975b7c2486ca
midimurphdesigns <midimurphdesigns@gmail.com>
2018-02-24 10:24:55 -0700

    took out all the console logs

--- index.js
+++ index.js
@@ -10,28 +10,26 @@ const STORE = {
   sortBy: 'alpha',
 };
 
-// dynamically create items for shopping list
-
 // <span class="timestamp">${getTimeString(item.createdAt)}</span>
 
-function generateItemElement(item, itemIndex) {
-  return `
-  <li class = "js-item-index-element" data-item-index="${item.id}">
-    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
-    <div class="shopping-item-controls">
-      <button class="shopping-item-toggle js-item-toggle">
-        <span class="button-label">check</span>
-      </button>
-      <button class="shopping-item-delete js-item-delete">
-        <span class="button-label">delete</span>
-      </button>
-      <form id="js-shopping-list-edit-entry">
-        <button type="submit">Edit item</button>
-          <input type="text" name="shopping-list-edit-entry" class="js-shopping-list-edit-entry" placeholder="New title">
-      </form>
-    </div>
-  </li>
-  `;
+function generateItemElement(item, itemIndex, template) {
+    return `
+    <li class = "js-item-index-element" data-item-index="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+        <form id="js-shopping-list-edit-entry">
+          <button type="submit">Edit item</button>
+            <input type="text" name="shopping-list-edit-entry" class="js-shopping-list-edit-entry" placeholder="New title">
+        </form>
+      </div>
+    </li>
+    `;
 }
 
 // figure out what time to display
@@ -40,28 +38,14 @@ function generateItemElement(item, itemIndex) {
 //   return strftime('%b-%d %H:%M', time);
 // }
 
-// make a string of shopping list items
-
 function generateShoppingItemsString(shoppingList){
-  console.log("Generating Shopping list element");
-  
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
   return items.join('');
 }
 
-// render shopping list function to the DOM
-
-function renderShoppingList(list) {
-
-  console.log('`renderShoppingList` ran');
+function renderShoppingList() {
 
   let filteredItems = [ ...STORE.items ];
-
-  if (list) {
-    filteredItems = [ ...list];
-  }
-
-  console.log(filteredItems);
 
   switch(STORE.sortBy) {
     case 'alpha':
@@ -77,19 +61,13 @@ function renderShoppingList(list) {
   $('.js-shopping-list').html(shoppingListItemsString);
 }
 
-// add a new item to the shopping list (STORE ^^^)
-
 function addItemToShoppingList(itemName) {
-  console.log(`Adding "${itemName}" to shopping list`);
   STORE.items.push({name: itemName, checked: false, createdAt: new Date(), id: STORE.items.length + 1});
 }
-
-// create new item on shopping list 
 
 function handleNewItemSubmit() {
   $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    console.log('`handleNewItemSubmit` ran');
     const newItemName = $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
     addItemToShoppingList(newItemName);
@@ -98,11 +76,8 @@ function handleNewItemSubmit() {
 }
 
 function toggleCheckedForListItem(itemID) {
-  console.log('Toggling checked property for item at index ' + itemID);
   STORE.items.find(item => item.id === itemID).checked = !STORE.items.find(item => item.id === itemID).checked;
 }
-
-// figure out what the index of the item is in STORE
 
 function getItemIndexFromElement(item) {
   const itemIndexString = $(item)
@@ -111,41 +86,29 @@ function getItemIndexFromElement(item) {
   return parseInt(itemIndexString, 10);
 }
 
-// check off items on shopping list
-
 function handleItemCheckClicked() {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
-    console.log('`handleItemCheckClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     toggleCheckedForListItem(itemIndex);
     renderShoppingList();
   });
 }
 
-// delete an item from STORE
-
 function removeItemFromList(itemID) {
   STORE.items = STORE.items.filter(item => item.id !== itemID);
 }
 
-// handle delete item from shopping list
-
 function handleDeleteItemClicked() {
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
-    console.log('`handleItemDeleteClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     removeItemFromList(itemIndex);
     renderShoppingList();
   });
 }
 
-// change sort by value of STORE
-
 function changeSortBy(sortBy) {
   STORE.sortBy = sortBy;
 }
-
-// handle sorting
 
 function handleSortChange() {
   $('#sort-options').change(event => {
@@ -158,16 +121,11 @@ function handleSortChange() {
   });
 }
 
-// search for item
-
 function itemSearch() {
   let item = $('.js-shopping-list-search-entry').val();
-  console.log(item);
   let searchItems = STORE.items.filter(val => val.name === item);
   return searchItems;
 }
-
-// handle items search
 
 function handleItemSearch() {
   $('#js-shopping-list-search').submit(event => {
@@ -177,9 +135,19 @@ function handleItemSearch() {
   });
 }
 
+function editItem() {
+  let item = $('.js-shopping-list-edit-entry').val();
+  let editItem = STORE.items.filter(val => val.name === item);
+  return editItem;
+}
 
-
-// run all the functions
+function handleEditItem() {
+  $('#js-shopping-list-edit-entry').submit(event => {
+    event.preventDefault();
+    let editItem = editItem();
+    renderShoppingList(editItem);
+  });
+}
 
 function handleShoppingList() {
   renderShoppingList();
@@ -188,6 +156,7 @@ function handleShoppingList() {
   handleDeleteItemClicked();
   handleSortChange();
   handleItemSearch();
+  handleEditItem();
 }
 
 handleShoppingList();
